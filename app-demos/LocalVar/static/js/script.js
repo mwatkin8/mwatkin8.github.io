d83f54646921efbe7fb5d2b8c8aac20899c33dbd
mwatkin8 <michael.watkins.8@gmail.com>
2021-05-13 21:13:55 -0600

    add padding

--- app-demos/LocalVar/static/js/script.js
+++ app-demos/LocalVar/static/js/script.js
@@ -179,7 +179,7 @@ async function fillTable(id){
                         document.getElementById('cv-int').innerHTML = bins[f]["ClinVar Interpretation"]
                         syn = ''
                         for(let i=0; i < bins[f]["ClinVar Synonyms"].length; i++){
-                            syn += bins[f]["ClinVar Synonyms"][i]["HGVS"] + "<br>"
+                            syn += bins[f]["ClinVar Synonyms"][i]["HGVS"] + "<br class=\"pb-2\">"
                         }
                         document.getElementById('cv-syn').innerHTML = syn
                     }
